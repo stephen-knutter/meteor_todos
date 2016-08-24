f01ba222ac5133f749b6b31cd203e2f76ea811e5
Stephen Knutter <stephen.knutter@gmail.com>
2016-08-24 10:47:19 -0600

    init

---
+++ imports/components/todosList/todosList.js
@@ -0,0 +1,73 @@
+
+import angular from 'angular';
+import angularMeteor from 'angular-meteor';
+import { Tasks } from '../../api/tasks.js';
+
+import template from './todosList.html';
+
+class TodosListCtrl {
+  constructor($scope) {
+    $scope.viewModel(this);
+
+    this.hideCompleted = false;
+
+    this.helpers({
+      tasks(){
+        const selector = {};
+
+        if(this.getReactively('hideCompleted')){
+          selector.checked = {
+            $ne: true
+          };
+        }
+
+        return Tasks.find(selector, {
+          sort: {
+            createdAt: -1
+          }
+        });
+      },
+      incompleteCount(){
+        return Tasks.find({
+          checked: {
+            $ne: true
+          }
+        }).count();
+      },
+      currentUser(){
+        return Meteor.user();
+      }
+    })
+  }
+
+  addTask(newTask){
+    Tasks.insert({
+      text: newTask,
+      createdAt: new Date,
+      owner: Meteor.userId(),
+      username: Meteor.user().username
+    });
+
+    this.newTask = '';
+  }
+
+  setChecked(task){
+    Tasks.update(task._id, {
+      $set: {
+        checked: !task.checked
+      },
+    });
+  }
+
+  removeTask(task){
+    Tasks.remove(task._id);
+  }
+}
+
+export default angular.module('todosList', [
+  angularMeteor
+])
+  .component('todosList', {
+    templateUrl: 'imports/components/todosList/todosList.html',
+    controller: ['$scope', TodosListCtrl]
+  });
